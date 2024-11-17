05532d674c5733155f2b32c4f28bb715b4621124
Prajakta Pikale <ppikale2@illinois.edu>
2024-11-17 17:23:30 -0600

    Merge with develop

--- select-star-frontend/src/App.js
+++ select-star-frontend/src/App.js
@@ -1,5 +1,7 @@
 
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Header from './components/Header/Header';
 // import Sidebar from './components/Sidebar/Sidebar';
@@ -7,6 +9,30 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import InventoryAnalysis from './components/InventoryAnalysis/InventoryAnalysis';
 
 function App() {
+  const [helloMessage, setHelloMessage] = useState('');
+  const [worldMessage, setWorldMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/hello')
+      .then(response => {
+        setHelloMessage(response.data.message);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the 'hello' message!", error);
+      });
+
+    axios.get('http://127.0.0.1:8000/world')
+      .then(response => {
+        setWorldMessage(response.data.message);
+      })
+      .catch(error => {
+        console.error("There was an error fetching the 'world' message!", error);
+      });
+  }, []);
+
+ console.log(helloMessage);
+ console.log(worldMessage);
+
   return (
     <Router>
     <div className="app">
