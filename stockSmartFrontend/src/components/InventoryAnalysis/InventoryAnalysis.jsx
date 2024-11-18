457fde9f37160641aa99b657b426b308b5ee585f
mbadve2 <mbadve2@illinois.edu>
2024-11-18 15:54:52 -0600

    Merge frontend changes

--- stockSmartFrontend/src/components/InventoryAnalysis/InventoryAnalysis.jsx
+++ stockSmartFrontend/src/components/InventoryAnalysis/InventoryAnalysis.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './InventoryAnalysis.css';
 import Sidebar from '../Sidebar/Sidebar'
 
 const InventoryAnalysis = () => {
+
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Fetch data from the backend
+    const fetchExpiringProducts = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:8000/top15ProductsExpiringSoon"); // API endpoint
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
+        setProducts(data); // Set fetched data
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchExpiringProducts();
+  }, []);
+
   return (
     <div className="inventory-analysis-container">
-        <Sidebar />
+        {/* <Sidebar /> */}
       <div className="content">
         <main className="main-content">
           <h2 className="main-title">Current Inventory Analysis</h2>
@@ -28,7 +51,39 @@ const InventoryAnalysis = () => {
           </div>
         </main>
       </div>
+      <div style={{ padding: "20px" }} className='queryTable'>
+     <h2>Products Expiring Soon</h2>
+     {isLoading ? (
+       <p>Loading...</p>
+     ) : (
+       <table border="1" style={{ width: "100%", borderCollapse: "collapse" }}>
+         <thead>
+           <tr>
+           <th>Serial No.</th>
+             <th>Product Name</th>
+             <th>Expiry Date</th>
+             <th>Supplier Name</th>
+             <th>Quantity to Replace</th>
+           </tr>
+         </thead>
+         <tbody>
+           {products.map((product, index) => (
+             <tr key={index}>
+              <td>{index+1}</td>
+
+               <td>{product.ProductName}</td>
+               <td>{product.ExpiryDate}</td>
+               <td>{product.SupplierName}</td>
+               <td>{product.QuantityOfProductsGettingExpired}</td>
+             </tr>
+           ))}
+         </tbody>
+       </table>
+     )}
+      </div>
+
     </div>
+
   );
 };
 
