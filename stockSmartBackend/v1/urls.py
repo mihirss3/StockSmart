63399b3d5609c46c701a36e2b5562720f86b0d6d
mbadve2 <mbadve2@illinois.edu>
2024-12-06 02:26:32 -0600

    Implement login backend

--- stockSmartBackend/v1/urls.py
+++ stockSmartBackend/v1/urls.py
@@ -3,7 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('products', ProductView.as_view(), name='products'),
-    path('productsCount', ProductCountView.as_view(), name='productsCount'),
-    path('top15ProductsExpiringSoon', Top15ProductsExpiringSoonView.as_view(), name='productsCount'),
+    path('login', LoginView.as_view(), name='login'),
 ]
--- stockSmartBackend/v1/views.py
+++ stockSmartBackend/v1/views.py
@@ -1,33 +1,20 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from django.db import connection
+from v1.db_utils.utils import getFromDB
 
 class IndexView(APIView):
     def get(self, request):
         return Response({"message": "The backend is running fine"})
 
-class ProductView(APIView):
-    def get(self, request):
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT ProductId, Name, PackagingType, Weight FROM Product")
-            rows = cursor.fetchall()
-        products = [{"ProductId": row[0], "Name": row[1], "PackagingType": row[2], "Weight": row[3]} for row in rows]
-        return Response(products)
-
-class ProductCountView(APIView):
-    def get(self, request):
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT COUNT(*) FROM Product")
-            rows = cursor.fetchall()
-        productsCount = {"count": rows[0][0]}
-        return Response(productsCount)
-
-class Top15ProductsExpiringSoonView(APIView):
-    def get(self, request):
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT p.Name,ExpiryDate, s.Name, SUM(i.Quantity) AS QuantityOfProductsGettingExpired FROM Product p LEFT JOIN Supplier s ON p.SupplierId = s.SupplierId INNER JOIN Inventory i ON i.ProductId = p.ProductId GROUP BY 1,2,3 ORDER BY ExpiryDate DESC LIMIT 15")
-            rows = cursor.fetchall()
-        products = [{"ProductName": row[0], "ExpiryDate": row[1], "SupplierName": row[2], "QuantityOfProductsGettingExpired": row[3]} for row in rows]
-        return Response(products)
-
+class LoginView(APIView):
+    def post(self, request):
+        data = request.data
+        emailId = data.get("emailId")
+        password = data.get("password")
+        rows = getFromDB("""SELECT Type FROM User WHERE EmailId = %s AND Password = %s""", (emailId,password))
+        if not rows:
+            return Response({"loginSuccess": False})
+        else:
+            userType = rows[0][0]
+            return Response({"loginSuccess": True, "userType": userType})
 
